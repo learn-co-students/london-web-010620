c5cfa3b60a1168c9bc391cafdc116e0555731f5b
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-24 10:53:25 +0000

    toy tale index formatting

--- 28-toy-tale-review/src/index.js
+++ 28-toy-tale-review/src/index.js
@@ -1,19 +1,18 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector("#new-toy-btn");
+const toyForm = document.querySelector(".container");
+let addToy = false;
 
 // YOUR CODE HERE
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   // hide & seek with the form
-  addToy = !addToy
+  addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = "block";
     // submit listener here
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = "none";
   }
-})
-
+});
 
 // OR HERE!
