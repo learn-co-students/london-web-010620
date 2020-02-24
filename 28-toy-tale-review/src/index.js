e24e876f79f696f571d590d818d85c16bef346dc
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-24 14:21:34 +0000

    toy tale

--- 28-toy-tale-review/src/index.js
+++ 28-toy-tale-review/src/index.js
@@ -1,18 +1,161 @@
 const addBtn = document.querySelector("#new-toy-btn");
-const toyForm = document.querySelector(".container");
+const toyFormContainer = document.querySelector(".container");
+const toyForm = document.querySelector("form");
+const toyCollection = document.querySelector("#toy-collection");
+const filterButton = document.querySelector("#likes-filter");
 let addToy = false;
+
+const TOYS_URL = "http://localhost:3000/toys";
+
+const API = {
+  getToys: () => fetch(TOYS_URL).then(res => res.json()),
+  postToy: toy =>
+    fetch(TOYS_URL, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(toy)
+    }).then(res => res.json()),
+  patchToy: toy =>
+    fetch(`${TOYS_URL}/${toy.id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "PATCH",
+      body: JSON.stringify(toy)
+    }).then(res => res.json()),
+  deleteToy: toy =>
+    fetch(`${TOYS_URL}/${toy.id}`, {
+      method: "DELETE"
+    }).then(res => res.json())
+};
 
 // YOUR CODE HERE
 
-addBtn.addEventListener("click", () => {
-  // hide & seek with the form
+const toggleToyFormVisibility = () => {
   addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = "block";
+    toyFormContainer.style.display = "block";
     // submit listener here
   } else {
-    toyForm.style.display = "none";
+    toyFormContainer.style.display = "none";
   }
-});
+};
+
+addBtn.addEventListener("click", toggleToyFormVisibility);
 
 // OR HERE!
+
+const ce = tag => document.createElement(tag);
+
+const renderToy = toy => {
+  const toyDiv = ce("div");
+
+  toyDiv.className = "card";
+
+  // h2 tag with the toy's name
+  const nameElement = ce("h2");
+  nameElement.innerText = toy.name;
+
+  // * image tag with the src of the toy's image attribute - needs a class name of "toy-avatar"
+  const imgElement = ce("img");
+  imgElement.className = "toy-avatar";
+  imgElement.src = toy.image;
+
+  // * p tag with how many likes that toy has
+  const likesElement = ce("p");
+  likesElement.innerText = `${toy.likes} likes`;
+
+  // * button tag with an class of "like-btn"
+  const likeButton = document.createElement("button");
+  likeButton.className = "like-btn";
+  likeButton.innerText = "Like <3";
+
+  likeButton.addEventListener("click", () => {
+    likeButton.disabled = true;
+    increaseLikes(toy).then(toy => {
+      likesElement.innerText = `${toy.likes} likes`;
+      likeButton.disabled = false;
+    });
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "like-btn";
+  deleteButton.innerText = "delete x";
+
+  deleteButton.addEventListener("click", () => deleteToy(toy, toyDiv));
+
+  toyDiv.append(
+    nameElement,
+    imgElement,
+    likesElement,
+    likeButton,
+    deleteButton
+  );
+
+  toyCollection.append(toyDiv);
+};
+
+const deleteToy = (toy, toyDiv) => {
+  API.deleteToy(toy).then(() => {
+    toyDiv.remove();
+  });
+};
+
+const increaseLikes = toy => {
+  toy.likes++;
+  return API.patchToy(toy);
+};
+
+const renderToys = toys => {
+  toyCollection.innerText = "";
+  toys.forEach(toy => {
+    renderToy(toy);
+  });
+};
+
+const createNewToyObject = (name, image) => {
+  return {
+    name,
+    image,
+    likes: 0
+  };
+};
+
+toyForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const newToy = createNewToyObject(
+    event.target.elements.name.value,
+    event.target.elements.image.value
+  );
+
+  API.postToy(newToy)
+    .then(toy => {
+      allToys.push(toy);
+      renderToy(toy);
+    })
+    .then(() => {
+      toggleToyFormVisibility();
+      event.target.reset();
+    });
+});
+
+let filterEnabled = false;
+let allToys = [];
+
+API.getToys().then(toys => {
+  allToys = toys;
+  filterButton.addEventListener("click", () => {
+    filterEnabled = !filterEnabled;
+    if (filterEnabled) {
+      renderToys(allToys.filter(toy => toy.likes >= 10));
+    } else {
+      renderToys(allToys);
+    }
+  });
+  renderToys(allToys);
+});
